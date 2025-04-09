45168c9695a09f68fc7738c97091d4b152c331f4
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-04-10 01:22:28 +0530

    updated the gameboard

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+const game = document.querySelector("#gameboard");
+const player = document.querySelector("#player");
+const startcells = ["","","","","","","","",""];
+
+player.textContent = " Player 1 goes First";
